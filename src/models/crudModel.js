4401263afbffdaa69a1646c9354bcf91bba6859b
VlV-515 <email@email.com>
2022-02-10 19:12:20 -0600

    Creadion de crudModel

---
+++ src/models/crudModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const crudSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  pass: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  dateIn: {
+    type: String,
+    required: true,
+  },
+});
+module.exports = mongoose.model("crudSchema", crudSchema);
