0eb99e8830a7c9913f294982961076ce9b6ff457
VlV-515 <email@email.com>
2022-02-17 13:15:42 -0600

    Setea los codigos de respuesta.

--- src/routes/crudRoute.js
+++ src/routes/crudRoute.js
@@ -5,23 +5,23 @@ const crudSchema = require("../models/crudModel");
 router.post("/", (req, res) => {
   crudSchema(req.body)
     .save()
-    .then(() => res.json({ msg: "ok" }))
-    .catch(() => res.json({ msg: "Error al insertar un usuario" }));
+    .then(() => res.status(200).json({ msg: "ok" }))
+    .catch(() => res.status(400).json({ msg: "Error al insertar un usuario" }));
 });
 //Read All
 router.get("/", (req, res) => {
   crudSchema
     .find()
-    .then((data) => res.json(data))
-    .catch(() => res.json({ msg: "Error al consultar todos los usuarios." }));
+    .then((data) => res.status(200).json(data))
+    .catch(() => res.status(400).json({ msg: "Error al consultar todos los usuarios." }));
 });
 //Read One
 router.get("/:id", (req, res) => {
   const { id } = req.params;
   crudSchema
     .findById(id)
-    .then((data) => res.json(data))
-    .catch(() => res.json({ msg: "Error obteniendo un usuario." }));
+    .then((data) => res.status(200).json(data))
+    .catch(() => res.status(400).json({ msg: "Error obteniendo un usuario." }));
 });
 //Update
 router.put("/:id", (req, res) => {
@@ -32,16 +32,16 @@ router.put("/:id", (req, res) => {
       { _id: id },
       { username, pass, name, lastname, email, role, dateIn }
     )
-    .then(() => res.json({ msg: "ok" }))
-    .catch(() => res.json({ msg: "Error editando el usuario" }));
+    .then(() => res.status(200).json({ msg: "ok" }))
+    .catch(() => res.status(400).json({ msg: "Error editando el usuario" }));
 });
 //Delete
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
   crudSchema
     .deleteOne({ _id: id })
-    .then(() => res.json({ msg: "ok" }))
-    .catch(() => res.json({ msg: "Error eliminando usuario" }));
+    .then(() => res.status(200).json({ msg: "ok" }))
+    .catch(() => res.status(400).json({ msg: "Error eliminando usuario" }));
 });
 
 module.exports = router;
